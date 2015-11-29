60eb75d7f24a47c9373f56152a741cca660e5977
Duc Duy <Duc Duy@DucDuy-PC>
2015-11-30 00:42:00 +0700

    Giao diện Server (chỉnh lại)

---
+++ src/chatClient/com/socket/ServerFrame.java
@@ -0,0 +1,151 @@
+package chatClient.com.socket;
+
+import java.awt.Color;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.io.File;
+import javax.swing.JFileChooser;
+import javax.swing.UIManager;
+
+public class ServerFrame extends javax.swing.JFrame {
+
+    public SocketServer server;
+    public Thread serverThread;
+    public String filePath = "D:/Data.xml";
+    public JFileChooser fileChooser;
+    
+    public ServerFrame() {
+        initComponents();     
+        jTextField3.setEditable(false);
+        jTextField3.setBackground(Color.WHITE);
+        
+        fileChooser = new JFileChooser();
+        jTextArea1.setEditable(false);
+    }
+    
+    public boolean isWin32(){
+        return System.getProperty("os.name").startsWith("Windows");
+    }
+
+    
+    private void initComponents() {
+
+        jButton1 = new javax.swing.JButton();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTextArea1 = new javax.swing.JTextArea();
+        jLabel3 = new javax.swing.JLabel();
+        jTextField3 = new javax.swing.JTextField();
+        jButton2 = new javax.swing.JButton();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Server");
+        
+        jScrollPane1.setBackground(Color.ORANGE);
+
+        jButton1.setText("Start Server");
+        jButton1.setBackground(Color.YELLOW);
+        jButton1.setForeground(Color.RED);
+        jButton1.setEnabled(false);
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
+        jTextArea1.setColumns(20);
+        jTextArea1.setFont(new java.awt.Font("Consolas", 0, 12)); 
+        jTextArea1.setRows(5);
+        jScrollPane1.setViewportView(jTextArea1);
+
+        jLabel3.setText("Database File : ");
+        jLabel3.setForeground(Color.RED);
+
+        jButton2.setText("Browse...");
+        jButton2.setBackground(Color.YELLOW);
+        jButton2.setForeground(Color.RED);
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane1)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addComponent(jLabel3)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(jTextField3, javax.swing.GroupLayout.DEFAULT_SIZE, 282, Short.MAX_VALUE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 91, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jButton1)))
+                .addContainerGap())
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jTextField3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel3)
+                    .addComponent(jButton2)
+                    .addComponent(jButton1))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 287, Short.MAX_VALUE)
+                .addContainerGap())
+        );
+
+        pack();
+    }
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {
+        server = new SocketServer(this);
+        jButton1.setEnabled(false); jButton2.setEnabled(false);
+    }
+
+    public void RetryStart(int port){
+        if(server != null){ server.stop(); }
+        server = new SocketServer(this, port);
+    }
+    
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {
+        fileChooser.showDialog(this, "Select");
+        File file = fileChooser.getSelectedFile();
+        
+        if(file != null){
+            filePath = file.getPath();
+            if(this.isWin32()){ filePath = filePath.replace("\\", "/"); }
+            jTextField3.setText(filePath);
+            jButton1.setEnabled(true);
+        }
+    }
+
+    public static void main(String args[]) {
+
+        try{
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        }
+        catch(Exception ex){
+            System.out.println("Look & Feel Exception");
+        }
+        
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new ServerFrame().setVisible(true);
+            }
+        });
+    }
+   
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JScrollPane jScrollPane1;
+    public javax.swing.JTextArea jTextArea1;
+    private javax.swing.JTextField jTextField3;
+}
